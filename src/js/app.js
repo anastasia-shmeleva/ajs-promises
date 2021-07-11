d27ae92e0a392fadda093e786b31f468cc20ddc3
Anastasia Shmeleva <shmeleva-nastya@mail.ru>
2021-07-11 12:49:41 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import GameSavingLoader from './gameSavingLoader';
+
+GameSavingLoader
+  .load()
+  .then((saving) => {
+    console.log(saving);
+  }, (error) => {
+    console.error(error);
+  });
